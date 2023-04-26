cce089aaf36ca015a7967ec09132c763d95ddbbc
Daniel <daniyus05@gmail.com>
2023-04-26 13:29:12 +0200

    intentos restantes

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numeroCorrecto = new Random().Next(1, 16); // Genera un número aleatorio entre 1 y 15
+int intentosRestantes = 3;
+
+while (intentosRestantes > 0)
+{
+    Console.Write("Adivina un número del 1 al 15: ");
+    int respuesta = Convert.ToInt32(Console.ReadLine());
+
+    if (respuesta == numeroCorrecto)
+    {
+        Console.WriteLine("¡Adivinaste!");
+        break;
+    }
+    else if (respuesta < numeroCorrecto)
+    {
+        Console.WriteLine("El número que buscas es mayor.");
+    }
+    else
+    {
+        Console.WriteLine("El número que buscas es menor.");
+    }
+
+    intentosRestantes--;
+    if (intentosRestantes > 0)
+    {
+        Console.WriteLine($"Te quedan {intentosRestantes} intentos.");
+    }
+    else
+    {
+        Console.WriteLine("¡Has perdido! El número correcto era " + numeroCorrecto);
+    }
+}
